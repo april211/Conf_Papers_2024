28aca49e4fe290b6e50f9ebab3d08d0fb4e2f2be
april211 <wuguangkai2018@outlook.com>
2024-05-22 11:01:49 +0800

    write script to sort all the keywords

---
+++ src/sort_keywords.py
@@ -0,0 +1,33 @@
+
+
+def load_keywords(path):
+    keywords = []
+    with open(path, "r") as f:
+        for line in f:
+            keywords.append(line.strip())
+    return keywords
+
+
+
+
+if __name__ == "__main__":
+
+    # load keywords
+    pos_keywords_path = "data/pos_keywords.txt"
+    neg_keywords_path = "data/neg_keywords.txt"
+    pos_keywords = load_keywords(pos_keywords_path)
+    neg_keywords = load_keywords(neg_keywords_path)
+
+    # sort the keywords
+    pos_keywords.sort()
+    neg_keywords.sort()
+
+    # save the sorted keywords
+    with open(pos_keywords_path, "w") as f:
+        for kw in pos_keywords:
+            f.write(kw + "\n")
+    
+    with open(neg_keywords_path, "w") as f:
+        for kw in neg_keywords:
+            f.write(kw + "\n")
+
