f63813c69ea9e8141756e31acfac0bb5913f38cd
april211 <wuguangkai2018@outlook.com>
2024-10-21 20:05:46 +0800

    add aug keywords: homophily, heterophily

--- src/sort_keywords.py
+++ src/sort_keywords.py
@@ -15,12 +15,15 @@ if __name__ == "__main__":
     # load keywords
     pos_keywords_path = "data/pos_keywords.txt"
     neg_keywords_path = "data/neg_keywords.txt"
+    aug_keywords_path = "data/aug_keywords.txt"
     pos_keywords = load_keywords(pos_keywords_path)
     neg_keywords = load_keywords(neg_keywords_path)
+    aug_keywords = load_keywords(aug_keywords_path)
 
     # sort the keywords
     pos_keywords.sort()
     neg_keywords.sort()
+    aug_keywords.sort()
 
     # save the sorted keywords
     with open(pos_keywords_path, "w") as f:
@@ -29,5 +32,9 @@ if __name__ == "__main__":
     
     with open(neg_keywords_path, "w") as f:
         for kw in neg_keywords:
+            f.write(kw + "\n")
+
+    with open(aug_keywords_path, "w") as f:
+        for kw in aug_keywords:
             f.write(kw + "\n")
 
